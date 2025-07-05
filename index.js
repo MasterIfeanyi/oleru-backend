dd5e5c6f425cb0be4da4239358657d6c28353f71
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2025-07-05 18:38:44 +0100

    add routes

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const corsOptions = require("./config/corsOptions");
 const cors = require('cors');
 require('dotenv').config();
+const router = require('./routes/route');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+
+app.use('/api', router);
 
 
 
